ec8cae42949d09dcde5352229c514619e8d309b7
Alap Raval <alapsraval@gmail.com>
2021-08-30 00:29:05 -0500

    sql formatting

--- employee_schema.sql
+++ employee_schema.sql
@@ -2,27 +2,30 @@ DROP DATABASE IF EXISTS employee_db;
 
 CREATE DATABASE employee_db;
 
-use employee_db;
+USE employee_db;
 
-create table department(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
-)
+CREATE TABLE department
+  (
+     id   INT NOT NULL auto_increment,
+     name VARCHAR(30) NOT NULL,
+     PRIMARY KEY(id)
+  );
 
-create table role(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(5,2) NOT NULL,
-    department_id INT default 0,
-    PRIMARY KEY(id)
-)
+CREATE TABLE role
+  (
+     id            INT NOT NULL auto_increment,
+     title         VARCHAR(30) NOT NULL,
+     salary        DECIMAL(5, 2) NOT NULL,
+     department_id INT DEFAULT 0,
+     PRIMARY KEY(id)
+  );
 
-create table employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY(id)
-)
+CREATE TABLE employee
+  (
+     id         INT NOT NULL auto_increment,
+     first_name VARCHAR(30) NOT NULL,
+     last_name  VARCHAR(30) NOT NULL,
+     role_id    INT NOT NULL,
+     manager_id INT,
+     PRIMARY KEY(id)
+  ); 
