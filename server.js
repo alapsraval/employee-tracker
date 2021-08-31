26f2306e558663bdf894df80f4f0dd8054579931
Alap Raval <alapsraval@gmail.com>
2021-08-30 22:34:54 -0500

    added db folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '',
-    database: 'employee_db',
+    database: 'employees_db',
 });
 
 connection.connect((err) => {
