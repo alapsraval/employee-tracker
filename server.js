868e66bb336da1f87f65ba0ed963b2c40b45ea5f
Alap Raval <alapsraval@gmail.com>
2021-08-31 00:18:45 -0500

    added env file and readEmployees function

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: '',
-    database: 'employees_db',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
+
+const readEmployees = () => {
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+};
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
+    readEmployees();
+    connection.end();
 });
