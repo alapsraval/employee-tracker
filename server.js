40ca5701b394ecd0796fe496e7ff7ce66c1718e8
Alap Raval <alapsraval@gmail.com>
2021-09-01 22:37:47 -0500

    get employees by manager function

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ let roles = [];
 let employees = [];
 let managers = [];
 
+//connection to MySQL
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -17,36 +18,87 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
-const showOptions = () => {
-    getRoles();
-    getEmployees();
-    getDepartments();
-
-    inquirer
-        .prompt({
-            name: 'crudOption',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: [
-                { name: 'View Employees', value: 'view-employee' },
-                { name: 'View Departments', value: 'view-department' },
-                { name: 'View Roles', value: 'view-role' },
-                { name: 'Add Employee', value: 'add-employee' },
-                { name: 'Add Department', value: 'add-department' },
-                { name: 'Add Role', value: 'add-role' },
-                // 'View Employees by Manager',
-                // 'Update Employee Role',
-                // 'Update Employee Manager'
-                { name: 'Quit', value: 'quit-app' }
-            ],
+// Helper functions
+const getEmployees = async () => {
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        employees = res.map(employee => {
+            return {
+                name: employee.first_name,
+                value: employee.id,
+            }
+        });
+        managers = res.filter(employee => employee.role_id === 1);
+        managers = managers.map(manager => {
+            return {
+                name: `${manager.first_name} ${manager.last_name}`,
+                value: manager.id
+            }
         })
+    });
+};
+
+const getDepartments = async () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        departments = res.map(department => {
+            return {
+                name: department.name,
+                value: department.id
+            }
+        });
+    });
+};
+
+const getRoles = async () => {
+    connection.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        roles = res.map(role => {
+            return {
+                name: role.title,
+                value: role.id
+            }
+        });
+    });
+};
+
+// show CRUD options
+const showOptions = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'crudOption',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: [
+                    { name: 'View Employees', value: 'view-employee' },
+                    { name: 'View Employees by Manager', value: 'view-employee-manager' },
+                    { name: 'View Departments', value: 'view-department' },
+                    { name: 'View Roles', value: 'view-role' },
+                    { name: 'Add Employee', value: 'add-employee' },
+                    { name: 'Add Department', value: 'add-department' },
+                    { name: 'Add Role', value: 'add-role' },
+                    { name: 'Update Employee Role', value: 'update-employee-role' },
+                    { name: 'Update Employee Manager', value: 'update-employee-manager' },
+                    { name: 'Quit', value: 'quit-app' }
+                ],
+            }, {
+                name: 'manager',
+                type: 'list',
+                message: 'Select the manager',
+                choices: managers,
+                when: (answers) => answers.crudOption === "view-employee-manager"
+            }
+        ])
         .then((answer) => {
             let crud = answer.crudOption.split("-")[0];
             let tableName = answer.crudOption.split("-")[1];
+            let filterName = answer.crudOption.split("-")[2];
+            let filterValue = answer.manager;
             let results = [];
             switch (crud) {
                 case "view":
-                    readRows(tableName);
+                    filterValue ? readRows(tableName, filterValue) : readRows(tableName);
                     break;
                 case "add":
                     if (tableName === 'employee') {
@@ -65,16 +117,30 @@ const showOptions = () => {
         });
 };
 
-// CRUD operations
+// CRUD functions
 
-const readRows = (tableName) => {
-    if (tableName === 'employee') {
+const readRows = (tableName, filterValue = null) => {
+    if (tableName === 'employee' && !filterValue) {
         connection.query(
             `SELECT e.first_name as First, e.last_name as Last, r.title as Title, d.name as Department, salary as Salary, concat(m.first_name, ' ', m.last_name) as Manager
             FROM employee as e
             LEFT JOIN role as r on e.role_id = r.id 
             LEFT JOIN department as d on r.department_id = d.id
             LEFT JOIN employee as m on e.manager_id = m.id`, (err, res) => {
+            if (err) throw err;
+            console.log(`\nAll Employees`);
+            console.log(`=============\n`);
+            console.table(res);
+            showOptions();
+        });
+    } else if (tableName === 'employee' && filterValue) {
+        connection.query(
+            `SELECT e.first_name as First, e.last_name as Last, r.title as Title, d.name as Department, salary as Salary, concat(m.first_name, ' ', m.last_name) as Manager
+            FROM employee as e
+            LEFT JOIN role as r on e.role_id = r.id 
+            LEFT JOIN department as d on r.department_id = d.id
+            LEFT JOIN employee as m on e.manager_id = m.id
+            WHERE e.manager_id = ${filterValue}`, (err, res) => {
             if (err) throw err;
             console.table(res);
             showOptions();
@@ -112,6 +178,7 @@ const createEmployee = () => {
                 type: 'list',
                 message: 'Employee\'s Manager: ',
                 choices: managers,
+                when: (answers) => answers.role_id !== 1
             },
         ])
         .then((answer) => {
@@ -193,52 +260,23 @@ const createRole = () => {
         });
 };
 
-// Helper functions
-const getEmployees = () => {
-    connection.query('SELECT * FROM employee', (err, res) => {
-        if (err) throw err;
-        employees = res.map(employee => {
-            return {
-                name: employee.first_name,
-                value: employee.id,
-            }
-        });
-        managers = res.filter(employee => employee.role_id === 1);
-        managers = managers.map(manager => {
-            return {
-                name: `${manager.first_name} ${manager.last_name}`,
-                value: manager.id
-            }
-        })
-    });
-};
-
-const getDepartments = () => {
-    connection.query('SELECT * FROM department', (err, res) => {
-        if (err) throw err;
-        departments = res.map(department => {
-            return {
-                name: department.name,
-                value: department.id
-            }
-        });
-    });
-};
-
-const getRoles = () => {
-    connection.query('SELECT * FROM role', (err, res) => {
-        if (err) throw err;
-        roles = res.map(role => {
-            return {
-                name: role.title,
-                value: role.id
-            }
-        });
-    });
-};
+let init = () => {
+    getEmployees();
+    getDepartments();
+    getRoles();
+    // added delay to load managers before loading prompts. 
+    // TODO: Replace with promise
+    setTimeout(() => {
+        showOptions();
+    }, 100);
+}
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    showOptions();
+    init();
+    // getRoles();
+    // getEmployees();
+    // getDepartments();
+
 });
